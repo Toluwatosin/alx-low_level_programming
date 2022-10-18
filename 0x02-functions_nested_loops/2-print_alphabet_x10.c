05fe48a7529279593ab04f5656bfb06d1183ca94
Toluwatosin <90407121+Toluwatosin@users.noreply.github.com>
2022-10-19 04:26:27 +0530

    Update 2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,13 +1,18 @@
 #include "main.h"
 /**
- * print_alphabet - prints out the stdout
+ * print_alphabet_x10 - prints the alphabet in ten times
  * Return: Nothing
  */
-void print_alphabet(void)
+void print_alphabet_x10(void)
 {
-	char c;
+	char c, i;
 
+	for (i = 0; i <= 9; i++)
+	{
 	for (c = 'a'; c <= 'z'; c++)
+	{
 	_putchar(c);
+	}
 	_putchar('\n');
+	}
 }
