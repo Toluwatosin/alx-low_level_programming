1f56f6c0b60d56e859dab6601ef46926cdc05a77
Toluwatosin <90407121+Toluwatosin@users.noreply.github.com>
2022-10-21 00:20:21 +0100

    Create 8-print_square.c

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * print_square - function declaration
+ * @size: assigned to the int
+ */
+void print_square(int size)
+{
+	int i, j;
+
+	if (size <= 0)
+	{
+	_putchar('\n');
+	}
+	else
+	{
+	for (i = 1; i <= size; i++)
+	{
+	for (j = 1; j <= size; j++)
+	{
+	_putchar('#');
+	}
+	_putchar('\n');
+	}
+	}
+}
