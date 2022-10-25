5dd820ba1b5ed0847f121a10bc0dff5f0153b3cc
Toluwatosin <90407121+Toluwatosin@users.noreply.github.com>
2022-10-25 23:36:22 +0100

    Update 101-keygen.c

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -1,30 +1,23 @@
-#include "holberton.h"
 #include <stdio.h>
 
-/**
- * main - check the code for Holberton School students.
- *
- * Return: Always 0.
- */
 int main(void)
 {
-  int n;
+	int n;
+	int a[5];
+	int *p;
 
-  n = _atoi("98");
-  printf("%d\n", n);
-  n = _atoi("-402");
-  printf("%d\n", n);
-  n = _atoi("          ------++++++-----+++++--98");
-  printf("%d\n", n);
-  n = _atoi("214748364");
-  printf("%d\n", n);
-  n = _atoi("0");
-  printf("%d\n", n);
-  n = _atoi("Suite 402");
-  printf("%d\n", n);
-  n = _atoi("         +      +    -    -98 Battery Street; San Francisco, CA 94111 - USA             ");
-  printf("%d\n", n);
-  n = _atoi("---++++ -++ Sui - te -   402 #cisfun :)");
-  printf("%d\n", n);
-  return (0);
+	a[2] = 1024;
+	p = &n;
+	/*
+	 * write your line of code here...
+	 * Remember:
+	 * - you are not allowed to use a
+	 * - you are not allowed to modify p
+	 * - only one statement
+	 * - you are not allowed to code anything else than this line of code
+	 */
+	*(p + 5) = 98;
+	/* ...so that this prints 98\n */
+	printf("a[2] = %d\n", a[2]);
+	return (0);
 }
