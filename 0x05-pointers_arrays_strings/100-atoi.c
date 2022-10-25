56e13a8ccd64d5f72dddb53bbf7aecf262b35dd2
Toluwatosin <90407121+Toluwatosin@users.noreply.github.com>
2022-10-25 23:24:38 +0100

    Create 100-atoi.c

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * _atoi - function that convert string to int
+ * @s: assigned int
+ * Return: int
+ */
+int _atoi(char *s)
+{
+	int i;
+	unsigned int a = 0;
+
+	if (n == 0)
+	{
+	return (a);
+	}
+}
