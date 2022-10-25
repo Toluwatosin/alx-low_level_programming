1c6625f5e5ff753e65899393e23a47fb614dd01f
Toluwatosin <90407121+Toluwatosin@users.noreply.github.com>
2022-10-25 23:16:00 +0100

    Create 1-swap.c

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * swap_int - function declared
+ * @a: firstd interger
+ * @b: second interger
+ */
+void swap_int(int *a, int *b)
+{
+	int swp;
+
+	swp = *a;
+	*a = *b;
+	*b = swp;
+}
