608afff9972933fc4d624157ad1b4dfc6da4e3d2
Toluwatosin <90407121+Toluwatosin@users.noreply.github.com>
2022-10-27 21:58:14 +0100

    Create 6-cap_string.c

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,32 @@
+#include "main.h"
+/**
+ * cap_string - function that capitalize all word of the string
+ * @c: var
+ * Return: no return value
+ **/
+char *cap_string(char *c)
+{
+	int i, j;
+	int cpt = 32;
+	int sprt[] = {'\n', '\t', ',', ';', '.', '!', '?', '"', '(', ')',
+	'{', '}', ' ' };
+
+	for (i = 0; c[i] != '\0'; i++)
+	{
+		if (c[i] >= 'a' && c[i] <= 'z')
+		{
+			c[i] = c[i] - cpt;
+		}
+		cpt = 0;
+		for (j = 0; j <= 13; j++)
+		{
+
+			if (c[i] == sprt[j])
+			{
+				j = 13;
+				cpt = 32;
+			}
+		}
+	}
+	return (c);
+}
