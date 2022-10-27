8686ca3abc2e0d23d85bc65fecc78591263a6c28
Toluwatosin <90407121+Toluwatosin@users.noreply.github.com>
2022-10-27 21:57:07 +0100

    Create 5-string_toupper.c

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * string_toupper - function that changes lower cases to uppercase
+ * @c: a pointer
+ * Return: 0
+ **/
+char *string_toupper(char *c)
+{
+	int i = 0;
+
+	while (c[i] != '\0')
+	{
+		if (c[i] >= 'a' && c[i] <= 'z')
+		{
+			c[i] = c[i] - 32;
+		}
+	i++;
+	}
+	return (c);
+}
