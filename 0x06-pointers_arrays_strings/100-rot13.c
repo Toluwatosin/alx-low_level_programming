b608be1c3dbc462c87c71b61e48cd622752d04a4
Toluwatosin <90407121+Toluwatosin@users.noreply.github.com>
2022-10-27 22:00:35 +0100

    Create 100-rot13.c

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * rot13 - function that encode string
+ * @c: char variable
+ * Return: 0 always
+ **/
+char *rot13(char *c)
+{
+	int i, j;
+
+	char a[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char b[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+
+	for (i = 0; c[i] != '\0'; i++)
+	{
+		for (j = 0; a[j] != '\0'; j++)
+		{
+			if (c[i] == a[j])
+			{
+				c[i] = b[j];
+				break;
+			}
+		}
+	}
+	return (c);
+}
