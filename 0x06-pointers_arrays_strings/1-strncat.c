46ec3c1c7a94c7cb6854a8bae945bc5bf5e3e4d0
Toluwatosin <90407121+Toluwatosin@users.noreply.github.com>
2022-10-27 21:53:13 +0100

    Create 1-strncat.c

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+ * _strncat - function declared to print two concatenated strings
+ * @dest: assigned char variable
+ * @src: assigned char variable
+ * @n: assigned int variable
+ * Return: is dest
+ **/
+char *_strncat(char *dest, char *src, int n)
+{
+	int l, j;
+
+	l = 0;
+	
+	while (dest[l] != '\0')
+	{
+	l++;
+	}
+
+	for (j = 0; j < n; j++)
+	{
+		dest[l + j] = src[j];
+		if (src[j] == '\0')
+		{
+		j = n;
+		}
+	}
+	return (dest);
+}
