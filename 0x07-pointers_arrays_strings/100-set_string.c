16ce02525937be6ab643a08023a157a133d874ff
Toluwatosin <90407121+Toluwatosin@users.noreply.github.com>
2022-10-31 22:57:41 +0100

    Create 100-set_string.c

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * set_string - sets the value of a pointer to a char
+ * @s: source address
+ * @to: targest\\t address
+ * Return: no return value
+ **/
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
