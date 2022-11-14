caee05ad5ff9c1cedd3da31be3304ba810cabd49
Toluwatosin <90407121+Toluwatosin@users.noreply.github.com>
2022-11-15 06:43:06 +0700

    Create 4-sum.h

---
+++ 0x0D-preprocessor/4-sum.h
@@ -0,0 +1,13 @@
+#ifndef SUM_H
+#define SUM_H
+
+/**
+ * define - function that sum two numbers
+ * SUM: macro function name
+* Desc: Header file that defines a function-like macro SUM(x, y)
+ *       that computes the sum of x and y.
+ */
+
+#define SUM(x, y) ((x) + (y))
+
+#endif
